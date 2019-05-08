d3eaddf689b955a4cda73fafae9030814980a0c6
Blanca Expósito <36692750+blancaexpositogonzalez@users.noreply.github.com>
2019-05-08 12:06:51 +0200

    Update Prueba 1.c

--- Prueba 1.c
+++ Prueba 1.c
@@ -1,21 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-// TITULO
-//void titulo();
+#define _CRT_SECURE_NO_WARNINGS
+#define NALUMNOS 100 //Número total de alumnos que caben en la biblioteca
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<Windows.h>
+
+struct Talumno { //Estructura que guarda los datos de cada alumno
+	char matricula[5];
+	int sesion; //1-ABIERTA 0-CERRADA
+
+};
+
+struct TAux { //Auxiliar para coger datos
+	char matricula[5];
+};
+
+//FUNCIONES
+//-> Funciones para actualizar ficheros:
+void actualizarFicheroUsuarios(int dim,int alumno,struct Talumno alumnos[]);
+//->Funciones para que los datos introducidos sean validos:
+void matriculaValida(char matricula[5]);
 
 int main()
 {
 	system("color 7C");
-	struct Biblioteca
-		{
-		char matricula[5];
- 		};
- 		
+
 	int n;
-	char A, B, op1, op2, op3;
+	int j, k = 0, i, comp1, comp2, auxx = 0, nusu = 0; // case 'E': variables para el registro y entrada de usuarios
+	char A, a, B, b, op1, op2, op3;
 	//Siendo A entrada, B salida.
-	char E, L, D, I;
+	char E, e, L, l, D, d, C, c;
 	//Siendo E estudio, L coger o dejar libro, D descanso, I información.
+	struct Talumno usu[NALUMNOS];
+	struct TAux aux; //Aux para el registro de entrada y salida de los usuarios
 	
 	printf("Elige una opcion.\n");
 	printf("A - entrar\n");	
@@ -24,31 +41,57 @@ int main()
 	switch(op1)
 	{
 		case 'A':
+		case 'a':
 			printf("Bienvenidx, que vienes a hacer?\n");
-			printf("E - Estudio\n");  
-			printf("L - Libro\n");
-			printf("I - Informacion\n");
+			printf("E - Sala de estudio\n"); 
+			printf("L - Dejar, coger o renovar un libro\n");
+			printf("C - Solicitar iformacion\n");
 			scanf(" %c", &op2);
 			switch(op2)
 			{
 				case 'E':
-					printf("Quedan %d plazas disponibles.\n", n);
-					if(n == 0)
-					{
-						printf("Los alumnos suelen estar un promedio de 3 horas. Disculpe las molestias.\n");
-					}
-					else
-					{
-						printf("Introduzca su numero de matricula.\n");
-						//Guardar los números de matrícula en un fichero.
-						//Habría un for para contabilizar el número de plazas ocupadas.
-						printf("Esperamos que aproveche su estudio.\n");
-					}
-					break;
+				case 'e':
+					if (usu[k].sesion == 1) {
+						printf("La sesion de %s esta abierta, salga para registrar un nuevo usuario\n", usu[k].matricula);
+						Sleep(10);
+				}
+				
+					else {
+	printf("Para entrar necesitamos su numero de matricula\n", usu[k].matricula);
+	do {
+			printf("\tIntroduzca su numero de matricula:\n");
+							matriculaValida(aux.matricula);
+							getchar();
+		for(k = 0; k < nusu; k++) {
+			comp1 = strcmp(aux.matricula, usu[k].matricula); //Compara entre todos los usuarios ya registrados
+			if(comp1 == 0) {
+				break;
+			}
+		}
+		
+		if(comp1 != 0) { //Si no hay nadie más registrado con ese número de matrícula
+		
+			if(nusu == NALUMNOS) { //Si el numero de alumnos es igual al número máximo de ellos
+			printf("\tNo queda sitio en la biblioteca, no pueden entrar mas alumnos\n");
+			}
+			else {
+				strcpy(usu[nusu].matricula, aux.matricula); //Guarda la matrícula nueva en los alumnos
+				
+				printf("\n\t>Usted ha sido registradx como: \n\t-%s\n\t", usu[nusu].matricula);
+			}
+		}
+	}
+	
+	while (comp1 == 0); //Se repite mientras se introduzcan matrículas ya registrada
+	
+}
+	
 				case 'L':
+				case 'l':
 					printf("Mas adelante encontrara un ordenador para devolver, renovar o buscar libros.\n");
 					break;
-				case 'I':
+				case 'C':
+				case 'c':
 					printf("Mas adelante encontrara un puesto de informacion.\n");
 					break;
 				default:
@@ -60,6 +103,7 @@ int main()
 		
 			
 		case 'B':
+		case 'b':
 			printf("Que deja de hacer?\n");
 			printf("E - Estudio\n");
 			printf("D - Descanso de estudio\n");
@@ -69,12 +113,14 @@ int main()
 			switch(op3)
 			{
 				case 'E':
+				case 'e':
 					printf("Introduzca su numero de matricula.\n");
 					//Quitar su numero de matricula del fichero.
 					printf("Esperamos que haya aprovechado su dia\n");
 					//Habría un for para contabilizar el número de plazas disponibles.
 					break;
 				case 'D':
+				case 'd':
 					printf("Introduzca su numero de matricula.\n");
 					//Guardar los numeros de matricula en otro fichero.
 					printf("Procura no tardar mas de 30 minutos, es posible que su sitio quede libre en caso contrario.\n");
@@ -82,9 +128,11 @@ int main()
 					//Si no regresa en 30 minutos, se quitará su matrícula del fichero. Así se desocupará su sitio.
 					break;
 				case 'L':
+				case 'l':
 					printf("Esperamos haberle servido de ayuda.\n");
 					break;
-				case 'I':
+				case 'C':
+				case 'c':
 					printf("Esperamos haberle servido de ayuda.\n");
 					break;
 				default:
@@ -99,6 +147,41 @@ int main()
 			printf("Lo siento, esa opcion no esta disponible.\n");
 			break;
 	}
+
+	
+}
+
+void matriculaValida(char matricula[5]) {
+	int i, contador, longitud, final;
+
+	do {
+		scanf("%s", matricula, 5);
+		longitud = strlen(matricula);
+		if (longitud != 5) { //Si no tiene 5 números
+			final = -2;
+		}
+		else {
+			for (i = 0; matricula[i] != '\0'; i++) {
+				
+				if ((matricula[i]> 'a' || matricula[i]< 'z')&& (matricula[i]> 'A' || matricula[i]< 'Z')) { //Hay letras
+					final = -1;
+				}
+				else {
+					final = 1;
+					break;
+				}
+			}
+		}
+
+		if (final == -2) {
+			printf("La matricula debe tener de 5 caracteres, introduzca de nuevo un numero de matricula\n");
+		}
+		else if (final == -1) {
+			printf("La matricula no puede contener letras, introduzca de nuevo un numero de matricula\n");
+		}
+
+	} while (final != 1);
+}
 	void titulo() {
 /*
 printf(" ______  _________ ______   _       _________ _______ _________ _______  _______  _______        _______ _________ _______ _________ ______  _________\n");
