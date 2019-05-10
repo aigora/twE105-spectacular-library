5fbc3a98cc2bd22b1d2e81e1805ee23f1cd5a619
Blanca Expósito <36692750+blancaexpositogonzalez@users.noreply.github.com>
2019-05-10 12:58:51 +0200

    Update Prueba 1.c

--- Prueba 1.c
+++ Prueba 1.c
@@ -1,4 +1,4 @@
-/ES NECESARIO ESCOGER LA OPCION SALIR PARA QUE SE GUARDEN LOS DATOS
+//ES NECESARIO ESCOGER LA OPCION SALIR PARA QUE SE GUARDEN LOS DATOS
 #define _CRT_SECURE_NO_WARNINGS
 #define NALUMNOS 100 //Número total de alumnos que caben en la biblioteca
 #include<stdio.h>
@@ -23,7 +23,8 @@ void actualizarFicheroAlumnos(int dim, int alumno, struct Talumno alumnos[]);
 void matriculaValida(char matricula[5]);
 //->Funciones que contienen menus y logotipos
 int SesionAbierta(int dim, struct Talumno * alumnos); //1:ABIERTA 0:CERRADA
-void logo();
+int LOGOTIPO(struct Talumno alumno[]); //Logotipo inicial que tambien lee el fichero alumnos
+
 
 int main()
 {
@@ -38,6 +39,7 @@ int main()
 	struct Talumno usu[NALUMNOS];
 	struct TAux aux; //Aux para el registro de entrada y salida de los usuarios
 	
+	nusu = LOGOTIPO(usu); //Logotipo y obtenemos toda la informacion del fichero alumnos
 	printf("Elige una opcion.\n");
 	printf("A - entrar\n");	
 	printf("B - salir\n");
@@ -46,10 +48,10 @@ int main()
 	{
 		case 'A':
 		case 'a':
-			printf("Bienvenidx, que vienes a hacer?\n");
+			printf("Que vienes a hacer?\n");
 			printf("E - Sala de estudio\n"); 
 			printf("L - Dejar, coger o renovar un libro\n");
-			printf("C - Solicitar iformacion\n");
+			printf("C - Solicitar informacion\n");
 			scanf(" %c", &op2);
 			switch(op2)
 			{
@@ -70,12 +72,12 @@ int main()
 			getchar();
 			for(k = 0; k < nusu; k++) {//La k se queda como la variable del alumno que está dentro
 				comp1 = strcmp(aux.matricula, usu[k].matricula); //Compara entre todos los usuarios ya registrados
-				if(comp1 == 0) {
+				if(comp1 != 0) {
 				break;
 			}
 		}
 		
-		if(comp1 != 0) { //Si no hay nadie más registrado con ese número de matrícula
+		if(comp1 == 0) { //Si no hay nadie más registrado con ese número de matrícula
 		
 			if(nusu == NALUMNOS) { //Si el numero de alumnos es igual al número máximo de ellos
 			printf("\tNo queda sitio en la biblioteca, no pueden entrar mas alumnos\n");
@@ -83,8 +85,8 @@ int main()
 			}
 			
 			else {
-				strcpy(usu[nusu].matricula, aux.matricula); //guarda la matrícula nueva en alumnos
-				printf("\n\tUsted ha sido registradx como: %d\n", usu[nusu].matricula);
+				strcpy_s(usu[nusu].matricula, 5, aux.matricula); //guarda la matrícula nueva en alumnos
+				printf("Usted ha sido registradx como: %i\n", usu[nusu].matricula);
 				system("pause");
 				usu[nusu].sesion = 1;
 				k = nusu;									
@@ -98,11 +100,10 @@ int main()
 				printf("Ya existe un usuario con ese nombre, intente con otro\n");
 			}
 		}
-	
-			while (comp1 == 0); //Se repite mientras se introduzcan matrículas ya registrada
-	
-}
+				while (comp1 == 0); //Se repite mientras se introduzcan matrículas ya registrada
+	}
 		Sleep(10);
+		
 		break;
 	
 				case 'L':
@@ -283,7 +284,9 @@ int SesionAbierta( int dim, struct Talumno * alumnos)  {
 	return opcion;
 }
 
-void logo() {
+int LOGOTIPO(struct Talumno alumno[])
+{
+
 printf(" ______  _________ ______   _       _________ _______ _________ _______  _______  _______        _______ _________ _______ _________ ______  _________\n");
 printf("(  ___ )  __   __/(  ___ ) ( )       __   __/(  ___  ) __   __/(  ____/ (  ____/ (  ___  )      (  ____/  __   __/(  ____/  __   __/(  __     __   __/\n");
 printf("| (   ) )   ) (   | (   ) )| |         ) (   | (   ) |   ) (   | (      | (      | (   ) |      | (         ) (   | (         ) (   | (     )   ) (   \n");
@@ -292,4 +295,11 @@ printf("|  __ (     | |   |  __ (  | |         | |   | |   | |   | |   |  __)   
 printf("| (    )    | |   | (    ) | |         | |   | |   | |   | |   | (      | |      | (   ) |      | (         | |         ) |   | |   | |   ) |   | |   \n");
 printf("| )___) )___) (___| )___) )| (_____ ___) (___| (___) |   | |   | (_____ | (_____ | )   ( |      | (_____    | |    _____) |___) (___| (__/  )___) (___\n");
 printf("|/  ___/  _______/|/  ___/ (_______/ _______/(_______)   )_(   (_______/(_______/|/      |      (_______/   )_(   /_______) _______/(______/  _______/\n");
+
+	FILE*fichero;
+	errno_t error;
+	printf("\n\t~~Bienvenido a la biblioteca de la ETSIDI~~\n\n");
+
+	printf("...Estamos cargando los datos del sistema\n");
+	Sleep(1500);
 }
